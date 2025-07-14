3e1bc28cadea58161f91710ae8d97bfdc5ba54b1
Nikolas Lennon <lennon.nikolas@gmail.com>
2025-07-14 14:40:42 -0500

    Backend: Heroku API deploy

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const path = require('path')
 
 const app = express()
 app.use(cors())
@@ -9,15 +8,6 @@ app.use(express.json())
 
 const checkRoute = require('./routes/check')
 app.use('/api', checkRoute)
-
-// Serve frontend static files in production
-if (process.env.NODE_ENV === 'production') {
-    const distPath = path.join(__dirname, 'frontend_dist');
-    app.use(express.static(distPath));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(distPath, 'index.html'));
-    });
-}
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
